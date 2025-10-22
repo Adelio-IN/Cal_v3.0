dd2c7ca110de8645cdd1c9b511cdf7fc8966233d
Adelio <2024011862@office.cju.ac.kr>
2025-10-22 15:10:48 +0900

    2025.10.22
    - ScientificCalculator 주석 추가 완료

--- Calculator/src/ScientificCalculator.java
+++ Calculator/src/ScientificCalculator.java
@@ -1,3 +1,5 @@
+// 계산기 v3.0 관련 클래스
+// https://github.com/Adelio-IN/Cal_v3.0/issues/2
 public class ScientificCalculator extends AdvancedCalculator {
     public int involution() {
         result = (int)Math.pow(num1, num2);
